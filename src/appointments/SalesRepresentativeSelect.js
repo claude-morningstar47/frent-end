d493a5e9d347b86385371909ae652a1c73c47bc2
Claude Emmanuel Mopeno-Bia <mopenobiaclaude@gmail.com>
2024-04-05 14:27:00 +0000

    Update SalesRepresentativeSelect.js

--- src/appointments/SalesRepresentativeSelect.js
+++ src/appointments/SalesRepresentativeSelect.js
@@ -20,6 +20,7 @@ const SalesRepresentativeSelect = ({ register }) => {
     "Manuel Romero",
     "Murphy Verger",
     "Simon Cadenne",
+    "Sophie Portail",
     "Sophie Rousmans",
     "Théo Raymond",
     "Vincen Le Mauff",
